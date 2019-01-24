dff33636e1d08fb2ff0ec67afb582b7ec1225a38
Payman Kossari <Paymankossari@gmail.com>
2019-01-24 12:21:51 -0500

    item module function updated

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,7 +1,22 @@
 'use strict';
 const Item = (function () {
-  const foo = 'bar';
-  return {
 
+  function validateName(name) {
+    if (name === null || name === undefined || name === '') {
+      throw new TypeError('Name does not exist.');
+    }
+  }
+
+  function create(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+    };
+  }
+
+  return {
+    validateName,
+    create
   };
 }());
